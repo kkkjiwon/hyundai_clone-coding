576f833e403cda3d1f2ae4d8f6df503caf5d2408
jiwon <wldnjs5391@gmail.com>
2024-02-29 13:14:41 +0900

    비지니스 부분 작업 중

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function () {
+  // AOS적용
+  AOS.init();
   // 언어 펼침 기능
   const langWord = document.querySelector(".language-word");
   const language = document.querySelector(".languge");
@@ -158,4 +160,13 @@ window.addEventListener("load", function () {
     // console.log(index);
     // console.log(item);
   });
+  // 비지니스 swiper
+  const swBusiness = new Swiper(".swBusiness" , {
+    loop:true, 
+    speed:500,
+    autoplay:{
+      delay:2500,
+      disableOnInteraction: false,
+    }
+  })
 });
